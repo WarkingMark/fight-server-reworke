e24971da8768fded0d7db62d6c04d47df6f04a15
WarkingMark <mark-trach@mail.ru>
2026-05-30 16:01:59 +0300

    fix

--- root@80.74.27.240/opt/game/fightd/fightc_1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-. ../../param.sh
-#while [ 1 ] ;
-  #do
-  #ulimit -c 100000
-    #./fclient -h ${GLOBALIP} -p${FIGHTCP} -l5 -c stop
-  #done
-
-# -d _fightc_4527.log
--- root@80.74.27.240/opt/game/fightd/fightd_1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-. ../../param.sh
-while [ 1 ] ;
-	do
-  ulimit -c 100000
-    ./fightd -h "127.0.0.1" -p ${FIGHTDP} -c ${FIGHTCP} -l "-1" -d _fightd.log -f ${HTTP}://${FIGHTDOMAIN}/private/fsfeedback.php
-  done
-
-# -d _fightd_5466.log
